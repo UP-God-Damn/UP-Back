e6c32ea73d50fa9e65fcf33688a7af55322a8c4a
soohyeon <qpsxhf410@outlook.kr>
2023-07-24 23:27:20 +0900

    🌿 CREATE :: ErrorCode 엔티티 작성

--- src/main/java/com/dsm/up/global/exception/ErrorCode.java
+++ src/main/java/com/dsm/up/global/exception/ErrorCode.java
@@ -1,4 +1,25 @@
 package com.dsm.up.global.exception;
 
-public class ErrorCode {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    PASSWORD_MISS_MATCH(400, "Password Miss Match"),
+
+    JWT_EXPIRED(401, "Jwt Expired"),
+
+    USER_NOT_FOUND(404, "User Not Found"),
+    POST_NOT_FOUND(404, "Post Not Found"),
+    COMMENT_NOT_FOUND(404, "Comment Not Found"),
+
+    ACCOUNTID_ALREADY_EXISTS(409, "AccountID Already Exists"),
+    NICKNAME_ALREADY_EXISTS(409, "Nickname Already Exists"),
+
+    SERVER_ERROR(500, "Server Error");
+
+    private final int status;
+    private final String message;
 }
