697288604aa86f160d4771ee36512b46169c0628
soohyeon <qpsxhf410@outlook.kr>
2023-07-26 22:19:48 +0900

    🌿 CREATE :: TokenErrorCode 작성

--- src/main/java/com/dsm/up/global/jwt/exception/error/TokenErrorCode.java
+++ src/main/java/com/dsm/up/global/jwt/exception/error/TokenErrorCode.java
@@ -1,4 +1,15 @@
 package com.dsm.up.global.jwt.exception.error;
 
-public class TokenErrorCode {
+import com.dsm.up.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TokenErrorCode implements ErrorCode {
+
+    TOKEN_UNAUTHORIZED(401,"토큰이 유효 하지 않습니다.");
+
+    private final int status;
+    private final String message;
 }
