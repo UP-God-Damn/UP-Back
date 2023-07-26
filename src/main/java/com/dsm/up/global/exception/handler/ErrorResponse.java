026c83fc5c2f0148a1c2546a48229219839f9ddd
soohyeon <qpsxhf410@outlook.kr>
2023-07-26 15:44:20 +0900

    🌿 CREATE :: ErrorResponse 작성

--- src/main/java/com/dsm/up/global/exception/handler/ErrorResponse.java
+++ src/main/java/com/dsm/up/global/exception/handler/ErrorResponse.java
@@ -1,4 +1,14 @@
 package com.dsm.up.global.exception.handler;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
+    private int status;
+    private String message;
+
+    protected ErrorResponse (int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 }
