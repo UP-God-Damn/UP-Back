43570361b50a345896f034979d8abdd09160ae4c
soohyeon <qpsxhf410@outlook.kr>
2023-07-26 22:20:19 +0900

    🌿 CREATE :: TokenUnauthorizedException 작성

--- src/main/java/com/dsm/up/global/jwt/exception/TokenUnauthorizedException.java
+++ src/main/java/com/dsm/up/global/jwt/exception/TokenUnauthorizedException.java
@@ -1,4 +1,12 @@
 package com.dsm.up.global.jwt.exception;
 
-public class TokenUnauthorizedException {
+import com.dsm.up.global.exception.BaseException;
+import com.dsm.up.global.jwt.exception.error.TokenErrorCode;
+
+public class TokenUnauthorizedException extends BaseException {
+    public final static TokenUnauthorizedException EXCEPTION = new TokenUnauthorizedException();
+
+    public TokenUnauthorizedException() {
+        super(TokenErrorCode.TOKEN_UNAUTHORIZED);
+    }
 }
