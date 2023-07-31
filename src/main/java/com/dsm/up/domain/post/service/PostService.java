23e0cb032860614db60de22905ef0b2366f2c4e4
soohyeon <qpsxhf410@outlook.kr>
2023-07-31 22:44:54 +0900

    🐚 UPDATE :: 코드 수정

--- src/main/java/com/dsm/up/domain/post/service/PostService.java
+++ src/main/java/com/dsm/up/domain/post/service/PostService.java
@@ -4,7 +4,9 @@ import com.dsm.up.domain.post.domain.Post;
 import com.dsm.up.domain.post.domain.repository.PostRepository;
 import com.dsm.up.domain.post.domain.type.MajorType;
 import com.dsm.up.domain.post.domain.type.StateType;
+import com.dsm.up.domain.post.exception.NotFoundException;
 import com.dsm.up.domain.post.presentation.dto.request.PostRequest;
+import com.dsm.up.global.exception.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,10 +29,10 @@ public class PostService {
                 .build()).getId();
     }
 
-    @Transactional
+    @Transactional // todo 수정하는 사람과 작성자가 일치하는 지 확인하기
     public Long update(Long id, PostRequest request) {
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 글 입니다"));
+                .orElseThrow(() -> new NotFoundException("존재히지 않는 게시글 입니다"));
         post.update(request.getTitle(), request.getContent(), request.getLanguage(), StateType.valueOf(request.getState()), MajorType.valueOf(request.getMajor()));
         return post.getId();
     }
