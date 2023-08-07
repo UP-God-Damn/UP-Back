e3b9cdb97ea4053569e52400a47afad9f548851b
soohyeon <qpsxhf410@outlook.kr>
2023-08-08 00:35:32 +0900

    🌿 CREATE :: comment delete 작성

--- src/main/java/com/dsm/up/domain/comment/presentation/CommentController.java
+++ src/main/java/com/dsm/up/domain/comment/presentation/CommentController.java
@@ -4,6 +4,7 @@ import com.dsm.up.domain.comment.presentation.dto.request.CommentRequest;
 import com.dsm.up.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,6 +32,12 @@ public class CommentController {
     @PutMapping("{id}")
     public Long update(@PathVariable @NotNull Long id, @RequestBody @Valid CommentRequest request) {
         return commentService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable @NotNull Long id) {
+        commentService.delete(id);
     }
 
 }
