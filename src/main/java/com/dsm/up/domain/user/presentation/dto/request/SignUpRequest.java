c955b8703e09d755a08200435f6f10c541d530da
lee na kyung <ni1722650@gmail.com>
2023-08-09 22:06:23 +0900

    🗑 DELETE :: 비밀번호 확인삭제

--- src/main/java/com/dsm/up/domain/user/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/dsm/up/domain/user/presentation/dto/request/SignUpRequest.java
@@ -18,8 +18,4 @@ public class SignUpRequest {
     @NotBlank
     @Size(min = 6, max = 20)
     private String password;
-
-    @NotBlank
-    @Size(min = 6, max = 20)
-    private String confirmPassword;
 }
