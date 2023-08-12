3313a96247ef3a80e0e8cf10f845f9e868c58d62
lee na kyung <ni1722650@gmail.com>
2023-08-13 07:32:49 +0900

    🐚 UPDATE :: JwtTokenProvider 수정

--- src/main/java/com/dsm/up/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/dsm/up/global/jwt/JwtTokenProvider.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import java.util.Base64;
 import java.util.Date;
-
+//뭐지..?
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
