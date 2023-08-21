95e1c359ce88af8c4a4480e3b36821757718936e
velyn666 <ni1722650@gmail.com>
2023-08-21 23:32:30 +0900

    🐚 UPDATE :: 회원가입시 토큰 반환 로직

--- src/main/java/com/dsm/up/domain/user/service/SignUpService.java
+++ src/main/java/com/dsm/up/domain/user/service/SignUpService.java
@@ -4,7 +4,9 @@ import com.dsm.up.domain.user.domain.User;
 import com.dsm.up.domain.user.domain.repository.UserRepository;
 import com.dsm.up.domain.user.exception.UserIdExistsException;
 import com.dsm.up.domain.user.presentation.dto.request.SignUpRequest;
+import com.dsm.up.domain.user.presentation.dto.response.TokenResponse;
 import com.dsm.up.global.aws.S3Util;
+import com.dsm.up.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,17 +20,29 @@ public class SignUpService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final S3Util s3Util;
+    private final JwtTokenProvider jwtTokenProvider;
     @Value("${cloud.aws.s3.default-image}")
     private String defaultImage;
 
-    public void userSignUp(SignUpRequest request, MultipartFile file) {
-        if(userRepository.existsByAccountId(request.getAccountId())) throw UserIdExistsException.EXCEPTION;
+    public TokenResponse userSignUp(SignUpRequest request, MultipartFile file) {
+        if (userRepository.existsByAccountId(request.getAccountId())) {
+            throw new UserIdExistsException();
+        }
 
         User user = userRepository.save(User.builder()
-            .accountId(request.getAccountId())
-            .password(passwordEncoder.encode(request.getPassword()))
-            .nickname(request.getNickname())
-            .build());
+                .accountId(request.getAccountId())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .nickname(request.getNickname())
+                .build());
         user.updatePath(file != null ? s3Util.upload(file) : defaultImage);
+
+        String accessToken = jwtTokenProvider.generateAccessToken(user.getAccountId());
+        String refreshToken = jwtTokenProvider.generateRefreshToken(user.getAccountId());
+
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
     }
 }
+
