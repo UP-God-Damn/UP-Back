d05705ebf95142975df70b0c0705bfb3c2d62590
soohyeon <qpsxhf410@outlook.kr>
2023-09-18 22:12:12 +0900

    🐚 UPDATE :: MajorType 추가

--- src/main/java/com/dsm/up/domain/post/domain/type/MajorType.java
+++ src/main/java/com/dsm/up/domain/post/domain/type/MajorType.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum MajorType {
-    BACKEND("백엔드"), FRONTEND("프론트엔드"), ANDROID("안드로이드"), FLUTTER("플러터"), IOS("ios"), DEVOPS("devops"), EMBEDDED("임베디드"), ETC("기타");
+    BACKEND("백엔드"), FRONTEND("프론트엔드"), FLUTTER("플러터"), AOS("안드로이드"), IOS("ios"), DEVOPS("devops"), EMBEDDED("임베디드"), SECURITY("정보보안"), ETC("기타");
 
     private final String major;
 }
